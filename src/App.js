46dee7f0420b2b219576c50eba4232a5a72fc054
alexis villaraza <alexis.borja@gmail.com>
2020-02-24 15:04:44 -0700

    tested if episodes render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
 
-import { fetchShow } from './api/fetchShow';
+import { fetchShow } from "./api/fetchShow";
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
@@ -17,24 +16,11 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    fetchShow
-      .then(res => {
-        setShow(res.data);
-        setSeasons(formatSeasons(res.data._embedded.episodes));
-      });
-    }, []);
-
-    // const fetchShow = () => {
-    //   axios
-    //     .get(
-    //       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-    //     )
-    //     .then(res => {
-    //       setShow(res.data);
-    //       setSeasons(formatSeasons(res.data._embedded.episodes));
-    //     });
-    // };
-    // fetchShow();
+    fetchShow().then(res => {
+      setShow(res.data);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+    });
+  }, []);
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
