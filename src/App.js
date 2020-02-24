a5e12a62e60179d27296ad812cdf5bdef09e0072
alexis villaraza <alexis.borja@gmail.com>
2020-02-24 14:36:28 -0700

    moved axios

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
 
+import { fetchShow } from './api/fetchShow';
+
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
@@ -15,18 +17,24 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
-  }, []);
+    fetchShow
+      .then(res => {
+        setShow(res.data);
+        setSeasons(formatSeasons(res.data._embedded.episodes));
+      });
+    }, []);
+
+    // const fetchShow = () => {
+    //   axios
+    //     .get(
+    //       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    //     )
+    //     .then(res => {
+    //       setShow(res.data);
+    //       setSeasons(formatSeasons(res.data._embedded.episodes));
+    //     });
+    // };
+    // fetchShow();
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
